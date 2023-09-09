6637b239b14115a2c033d2004a72415bc2f52635
Micah Gober <micahmgober@gmail.com>
2023-09-08 18:48:48 -0700

    Made addBlob(String) and tested it

--- Index.java
+++ Index.java
@@ -1,14 +1,41 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Index 
 {
+    File index;
+    File folder;
     //public void initialize(String fileName, String folderName)
     public void initialize()
     {
-        File index = new File("index");
-        File folder = new File("objects");
+        index = new File("index");
+        folder = new File("objects");
         folder.mkdir();
     }
 
-    
+    public void addBlob(String fileName) throws IOException
+    {
+        Blob blob = new Blob();
+        String contents = blob.read(fileName);
+        String sha = blob.encryptPassword(contents);
+        blob.write();
+        String indexContents = "";
+        BufferedReader reader = new BufferedReader(new FileReader (index));
+        while (reader.ready())
+        {
+            indexContents += (char) reader.read();
+        }
+        reader.close();
+        PrintWriter writer = new PrintWriter (index);
+        writer.print(indexContents);
+        writer.println(fileName + " : " + sha);
+        writer.close();
+    }
+    public void removeBlob(String fileName)
+    {
+
+    }
 }
--- Tester.java
+++ Tester.java
@@ -4,9 +4,15 @@ public class Tester
 {
     public static void main (String [] args) throws IOException
     {
-        Blob blob = new Blob();
-        String contents = blob.read("Test.txt");
-        blob.encryptPassword(contents);
-        blob.write();
+        //Blob blob = new Blob();
+        //String contents = blob.read("Test.txt");
+        //blob.encryptPassword(contents);
+        //blob.write();
+        Index index = new Index();
+        index.initialize();
+        index.addBlob("addBlobTest.txt");
+        index.addBlob("addBlobTest2.txt");
+        index.addBlob("addBlobTest3.txt");
+        
     }
 }
